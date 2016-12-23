071fd27b7a0fce479bf15813e3845617798b0e7f
hanl1 <hanl1@yonyou.com>
2016-12-23 11:30:01 +0800

    1.第一个idea测试项目

---
+++ src/main/java/com/hanle/test/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.hanle.test;
+
+/**
+ * Created by apple on 16/12/23.
+ */
+public class HelloWorld {
+    public  static  void main(){
+        System.out.println("HelloWrold");
+    }
+}
---
+++ src/test/java/com/hanle/test/TestHelloWorld.java
@@ -0,0 +1,13 @@
+package com.hanle.test;
+
+import org.junit.Test;
+
+/**
+ * Created by apple on 16/12/23.
+ */
+public class TestHelloWorld {
+    @Test
+    public void test(){
+        System.out.println("HelloWorld");
+    }
+}
